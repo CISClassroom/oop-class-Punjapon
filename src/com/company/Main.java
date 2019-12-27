f8543b81571bc7c7a50fa98d841c3bc789290510
unknown <aueaoangkun@kkumail.com>
2019-12-27 22:45:40 +0700

    Hw

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Student Pun = new Student();
+        Pun.name = "นายปัญจพล อ่อนโคทา";
+        Pun.id = "623410006-2";
+        Pun.major = "วิทยาการคอมพิวเตอร์และสารสนเทศ";
+
+        System.out.println("1.ชื่อ " + Pun.name);
+        System.out.println("2.รหัสนักศึกษา " + Pun.id);
+        System.out.println("3.สาขา " + Pun.major);
     }
 }
